c0086d916ddfeef499f5b8bea66448845eef61ce
Lakshhiitaaa <124601979+Lakshhiitaaa@users.noreply.github.com>
2026-02-18 23:37:54 +0530

    Fix Google OAuth redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,41 @@
 "use client"
 
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 import { supabase } from "@/lib/supabaseClient"
 
 export default function Home() {
+  const router = useRouter()
 
-  const loginWithGoogle = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: {
-        redirectTo: "http://localhost:3000/dashboard",
-      },
-    })
-  }
+  // 🔐 Check session on load
+  useEffect(() => {
+    const checkUser = async () => {
+      const { data } = await supabase.auth.getUser()
+
+      if (data.user) {
+        router.push("/dashboard") // Redirect if logged in
+      }
+    }
+
+    checkUser()
+  }, [router])
+
+// Google Login
+const loginWithGoogle = async () => {
+  await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: "https://smart-bookmark-app-five-chi.vercel.app/dashboard",
+    },
+  })
+}
+
 
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
+    <div className="flex h-screen items-center justify-center">
       <button
         onClick={loginWithGoogle}
-        className="bg-black text-white px-6 py-3 rounded-lg hover:bg-gray-800"
+        className="bg-black text-white px-6 py-3 rounded-lg"
       >
         Sign in with Google
       </button>
